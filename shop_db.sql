421a4d4da503aa42583cb564a1b999f1bd672b98
Prakash Dass R <123339797+prakash02100@users.noreply.github.com>
2023-06-22 16:16:27 +0530

    Add files via upload

--- shop_db.sql
+++ shop_db.sql
@@ -1,17 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 30, 2022 at 12:51 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Generation Time: Jun 20, 2023 at 08:15 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
- 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -31,14 +31,15 @@ CREATE TABLE `admins` (
   `id` int(100) NOT NULL,
   `name` varchar(20) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `admins`
 --
 
 INSERT INTO `admins` (`id`, `name`, `password`) VALUES
-(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2');
+(1, 'admin', '6216f8a75fd5bb3d5f22b6f9958cdede3fc086c2'),
+(3, 'admin', '123');
 
 -- --------------------------------------------------------
 
@@ -54,7 +55,16 @@ CREATE TABLE `cart` (
   `price` int(10) NOT NULL,
   `quantity` int(10) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`id`, `user_id`, `pid`, `name`, `price`, `quantity`, `image`) VALUES
+(93, 1, 5, 'Imam Prasad Pack 2', 600, 1, 'alphonso-.webp'),
+(97, 2, 4, 'Imam Prasad', 200, 5, 'alphonso-.webp'),
+(98, 3, 5, 'Imam Prasad Pack 2', 600, 1, 'alphonso-.webp');
 
 -- --------------------------------------------------------
 
@@ -69,7 +79,14 @@ CREATE TABLE `messages` (
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
   `message` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `user_id`, `name`, `email`, `number`, `message`) VALUES
+(1, 1, 'test', 'test@gmail.com', '9077', 'Mess!');
 
 -- --------------------------------------------------------
 
@@ -88,10 +105,20 @@ CREATE TABLE `orders` (
   `total_products` varchar(1000) NOT NULL,
   `total_price` int(100) NOT NULL,
   `placed_on` date NOT NULL DEFAULT current_timestamp(),
-  -- `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
-  `payment_status` varchar(20) NOT NULL,
-  `delivery_status` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `payment_status` varchar(20) NOT NULL DEFAULT 'pending',
+  `delivery_status` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`, `delivery_status`) VALUES
+(28, 1, 'new1', '9077', 'test@gmail.com', 'online payment', 'flat no. w, w, w, w, India - 9', '[value-2] (600 x 1) - ', 600, '2023-06-15', 'completed', 'tomorrow'),
+(36, 1, 'red', '9077', 'test@gmail.com', 'online payment', 'flat no. w, w, w, w, India - 9', 'Imam Prasad (200 x 1) - ', 200, '2023-06-16', 'pending', 'By today'),
+(44, 2, 'new1', '9077', 'test@gmail.com', 'online payment', 'flat no. w, w, w, w, India - 9', 'Imam Prasad (200 x 1) - ', 200, '2023-06-16', 'pending', 'tomorrow'),
+(45, 2, 'new1', '9077', 'test@gmail.com', 'online payment', 'flat no. w, w, w, w, India - 9', 'Imam Prasad (200 x 1) - ', 200, '2023-06-16', 'pending', 'dispatched'),
+(46, 2, 'new1', '9077', 'test@gmail.com', 'cash on delivery', 'flat no. w, w, w, w, India - 9', 'Imam Prasad Pack 2 (600 x 2) - ', 1200, '2023-06-16', 'pending', NULL);
 
 -- --------------------------------------------------------
 
@@ -107,10 +134,18 @@ CREATE TABLE `products` (
   `image_01` varchar(100) NOT NULL,
   `image_02` varchar(100) NOT NULL,
   `image_03` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `details`, `price`, `image_01`, `image_02`, `image_03`) VALUES
+(4, 'Imam Prasad', 'Quantity 4 Mangoes', 200, 'alphonso-.webp', 'mango-.webp', 'alphonso-.webp'),
+(5, 'Imam Prasad Pack 2', 'Exclusive Pack of 6 Mangoes', 600, 'alphonso-.webp', 'mango-.webp', 'alphonso-.webp'),
+(6, 'Imam Prasad Pack 3', 'Exclusive Family Pack of 12 Mangoes', 1200, 'mango-.webp', 'alphonso-.webp', 'alphonso-.webp');
 
 -- --------------------------------------------------------
-
 
 --
 -- Table structure for table `users`
@@ -121,7 +156,16 @@ CREATE TABLE `users` (
   `name` varchar(20) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+(1, 't', 'test@gmail.com', '8efd86fb78a56a5145ed7739dcb00c78581c5375'),
+(2, 't1', 'test2@gmail.com', '8efd86fb78a56a5145ed7739dcb00c78581c5375'),
+(3, 'test', 'check1@gmail.com', 'd56d985300d4b52eb6e189be006f44f8d23c5ec9');
 
 -- --------------------------------------------------------
 
@@ -136,7 +180,7 @@ CREATE TABLE `wishlist` (
   `name` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -179,12 +223,6 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `wishlist`
---
-ALTER TABLE `wishlist`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -192,43 +230,37 @@ ALTER TABLE `wishlist`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `wishlist`
---
-ALTER TABLE `wishlist`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
